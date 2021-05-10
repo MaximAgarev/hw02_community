898d01a369ac722c596fd495fbcf355984b33ba5
MaximAgarev <maximagarev@yandex.ru>
2021-05-10 20:54:09 +0300

    Second review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -20,3 +20,6 @@ class Post(models.Model):
                                related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
                               related_name="posts", blank=True, null=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
